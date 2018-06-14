218d08e0c6f1f5f3c14bf6c786d7f64cd1a89c21
Anna Shavurska <a.shavurska@viseven.com>
2018-06-14 18:29:11 +0300

    update(config.js)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,22 +40,13 @@ module.exports = {
       },
       // TODO: Add Github link
       // Assumes GitHub. Can also be a full GitLab url.
-      repo: 'QAPInt/ewizardjs-docs',
-      // Customising the header label
-      // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-      repoLabel: 'Contribute!',
-
+      // repo: 'QAPInt/ewizardjs-docs',
       // Optional options for generating "Edit this page" link
-
       // if your docs are in a different repo from your main project:
       docsRepo: 'QAPInt/ewizardjs-docs',
-      // if your docs are not at the root of the repo:
       docsDir: 'docs',
-      // if your docs are in a specific branch (defaults to 'master'):
       docsBranch: 'master',
-      // defaults to false, set to true to enable
       editLinks: true,
-      // custom text for edit link. Defaults to "Edit this page"
       editLinkText: 'Help us improve this page!'
     }
   }
