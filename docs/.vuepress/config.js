fe0166467552244c45c7b7b119ebc7c171e6b150
Oleg Shevchuk <oleg.shevchuk@viseven.com>
2018-07-16 12:13:58 +0300

    fix(config.js): proper navigator link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
           '',
           'attributes',
           'component',
-         ['Navigator', 'navigator'] 
+          'navigator',
         ],
         '/release-notes/': [
           '',
